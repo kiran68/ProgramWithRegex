6a6d61c3cb45b1caa8381b311b57afe0b2bd9341
kiran68 <kiranurshil88@gmail.com>
2023-04-11 15:15:19 +0530

    Add: Check Valid Country Code With Mobile Number

--- JavaRegex.java
+++ JavaRegex.java
@@ -1,29 +1,25 @@
 package com.bridgelabz.ragex;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.Scanner;
 
+	import java.util.Scanner;
+	import java.util.regex.Matcher;
+	import java.util.regex.Pattern;
 
+	public class JavaRegex {
+		public static void main(String[] args) {
+			String regex = "^[1-9][0-9]\s[1-9][0-9]{9}$";
 
-public class JavaRegex {
-	public static void main(String[] args) {
-		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Mobile Number: ");
+			String mobileNO = sc.nextLine();
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Email Address: ");
-		String data = sc.nextLine();
-		
-
-		Pattern pattern = Pattern.compile(regex);
-
-		Matcher matcher = pattern.matcher(data);
-		boolean result = matcher.matches();
-	    
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(mobileNO);
+			boolean result = matcher.matches();
 	if (result) {
-		System.out.println("Valid Email");
-	} else {
-		System.out.println("Invalid Email");
+				System.out.println("Valid Mobile Number");
+			} else {
+				System.out.println("Invalid Mobile Number");
+			}
+		}
 	}
-	}
-}
