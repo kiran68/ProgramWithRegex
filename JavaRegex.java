67e5bb45a49f194d874ecdf76ad787e7cc58c05a
kiran68 <kiranurshil88@gmail.com>
2023-04-11 15:55:58 +0530

    Add: Password Atleast One Numeric Number

--- JavaRegex.java
+++ JavaRegex.java
@@ -1,28 +1,26 @@
 package com.bridgelabz.ragex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	import java.util.Scanner;
-	import java.util.regex.Matcher;
-	import java.util.regex.Pattern;
+public class JavaRegex {
+	public static void main(String[] args) {
 
-	public class JavaRegex {
-		public static void main(String[] args) {
+		String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 
-					String regex = "^(?=.*[A-Z]).{8,}$";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter A Password: ");
+		String password = sc.nextLine();
 
-					Scanner sc = new Scanner(System.in);
-					System.out.println("Enter A Password: ");
-					String password = sc.nextLine();
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
+		boolean result = matcher.matches();
 
-					Pattern pattern = Pattern.compile(regex);
-					Matcher matcher = pattern.matcher(password);
-					boolean result = matcher.matches();
-
-					if (result) {
-						System
-			.out.println("You have entered a Valid Password");
-					} else {
-						System.out.println("Invalid Password");
-					}
+		if (result) {
+			System.out.println("You have entered a Valid Password");
+		} else {
+			System.out.println("Invalid Password");
+		}
 	}
-	}
+}
