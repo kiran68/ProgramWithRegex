d998fe588c5e322ce82ac9a5fd57e9e874512a8a
kiran68 <kiranurshil88@gmail.com>
2023-04-11 16:09:33 +0530

    Add: Password Atleast One Special Character

--- JavaRegex.java
+++ JavaRegex.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class JavaRegex {
 	public static void main(String[] args) {
 
-		String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+		String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter A Password: ");
