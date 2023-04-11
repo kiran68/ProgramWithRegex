941d547518143987ebe6cd3c623897c6a1fc12df
kiran68 <kiranurshil88@gmail.com>
2023-04-11 14:58:57 +0530

    Add: Check Valid EMail

---
+++ JavaRegex.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.ragex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+
+
+public class JavaRegex {
+	public static void main(String[] args) {
+		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Email Address: ");
+		String data = sc.nextLine();
+		
+
+		Pattern pattern = Pattern.compile(regex);
+
+		Matcher matcher = pattern.matcher(data);
+		boolean result = matcher.matches();
+	    
+	if (result) {
+		System.out.println("Valid Email");
+	} else {
+		System.out.println("Invalid Email");
+	}
+	}
+}
